fbce01b5a5dd8c71c97d38fb4485d528913dc41a
Mariana Kreisig <marianakreisig@gmail.com>
2015-11-10 21:36:35 -0200

    feat(tasks): strike the description when the task is flagged as done

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -17,5 +17,12 @@ describe('thingsToBeDoneApp', function() {
       expect(element.all(by.css('.task input')).first().isSelected()).toBeTruthy();
       expect(element.all(by.css('.task input')).get(1).isSelected()).toBeFalsy();
     });
+
+    it('should strike the description when the task is done', function() {
+      element.all(by.css('.task input')).first().click();
+
+      expect(element.all(by.css('.task span')).first().getAttribute('class')).toMatch('striked');
+      expect(element.all(by.css('.task span')).get(1).getAttribute('class')).not.toMatch('striked');
+    });
   });
 });
